81191e5f52638a1a7e9a58ee2b0888ec8ade2115
OmarHeviaArbana <ohevia@uoc.edu>
2024-04-08 18:21:43 +0200

    core.js of d_every completed and tests passing (one pending)

--- PEC2_Ej3/d_every/core.js
+++ PEC2_Ej3/d_every/core.js
@@ -1,30 +1,28 @@
-// Check to see if all elements in an array
-// are even numbers.
-
 function allEven(input) {
-  return input;
+  return input.every(number => typeof(number) === 'number' && number % 2 === 0);
 }
-
-// Check to see if all elements in an array
-// are of the same type.
 
 function allSameType(input) {
-  return input;
+  const typeInput = typeof input[0];
+  return input.every(item => typeof(item) === typeInput);
 }
-
-// Check to see if every element in the matrix is
-// an array and that every element in the array is
-// greater than 0.
 
 function positiveMatrix(input) {
-  return input;
+  return input.every(arrayRow => arrayRow.every(number => number > 0));
 }
 
-// Check that all items in an array are strings
-// and that they all only contain the same vowels.
-
 function allSameVowels(input) {
-  return input;
+
+   const inputstring = input.every(item => typeof(item) === 'string');
+   if (inputstring) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const firstWordVowels = new Set(input[0].toLowerCase().split('').filter(char => vowels.includes(char)));
+    return input.every(word => {
+      const wordVowels = new Set(word.toLowerCase().split('').filter(char => vowels.includes(char)));
+     return JSON.stringify(firstWordVowels) === JSON.stringify(wordVowels);
+   });
+  }
+
 }
 
 module.exports = {
