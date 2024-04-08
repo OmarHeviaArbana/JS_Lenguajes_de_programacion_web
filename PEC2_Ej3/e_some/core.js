3c5922b85d76b847fe7190a3d74a2d730fff9fc7
OmarHeviaArbana <ohevia@uoc.edu>
2024-04-08 21:16:05 +0200

    core.js of e_some completed and tests passing

--- PEC2_Ej3/e_some/core.js
+++ PEC2_Ej3/e_some/core.js
@@ -2,28 +2,29 @@
 // array are numbers greater than 10.
 
 function anyGreaterThan10 (input) {
-  return input;
+  return input.some(number => number > 10);
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
 function longWord (input) {
-  return input;
+  return input.some(word => word.length > 10);
 };
 
 // Check to see if any of the elements in
 // the matrix are true.
 
 function truePossibilities (input) {
-  return input;
+  return input.some(arrayRow => arrayRow.includes(true));
 };
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
 function lostCarcosa (input) {
-  return input;
+  const target = "Strange is the night where black stars rise,";
+  return input.some(line => line.includes(target));
 };
 
 module.exports = {
